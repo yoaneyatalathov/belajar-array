fc019a2b86b06c28e5544fc5cbce2584dca10bad
Yoane Yatalathov <yatalathov.gamedev@gmail.com>
2023-03-11 22:54:16 +0700

    mengubah nilai index ke-4

--- source/buble.js
+++ source/buble.js
@@ -1,3 +1,6 @@
 let flavors = ["vanilla", "butterscotch", "lavender", "chocolate", "cookie dough"];
 
 let flavorOfTheDay = flavors[2];
+
+flavors[3] = "vanilla chocolate chip";
+console.log(flavors);
