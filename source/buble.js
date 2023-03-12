ebd65873ade4f5e1ba41a73bac2ed82a2faee4ba
Yoane Yatalathov <yatalathov.gamedev@gmail.com>
2023-03-12 23:30:18 +0700

    mengubah increment

--- source/buble.js
+++ source/buble.js
@@ -3,7 +3,7 @@ let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
               46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
 let output;
-for (var i = 0; i < scores.length; i = i + 1) {
+for (var i = 0; i < scores.length; i++) {
     output = "Bubble solution #" + i + " score : " + scores[i];
     console.log(output);
 }
