b9bb259e01a20c9be514edfc226d51a0dcdb11a1
Yoane Yatalathov <yatalathov.gamedev@gmail.com>
2023-03-12 22:27:24 +0700

    mengubah while loop menjadi for loop

--- source/buble.js
+++ source/buble.js
@@ -3,9 +3,7 @@ let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
               46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
 let output;
-let i = 0;
-while (i < scores.length) {
+for (var i = 0; i < scores.length; i = i + 1) {
     output = "Bubble solution #" + i + " score : " + scores[i];
     console.log(output);
-    i = i + 1;
 }
