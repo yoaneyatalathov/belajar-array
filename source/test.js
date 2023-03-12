4d3a6f075776304405de8cd43b0613e999f177e8
Yoane Yatalathov <yatalathov.gamedev@gmail.com>
2023-03-12 21:53:07 +0700

    menambahkan file test.js

---
+++ source/test.js
@@ -0,0 +1,11 @@
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+var hasBubbleGum = [false, false, false, true];
+var i = 0;
+
+while (i < hasBubbleGum.length) {
+    if (hasBubbleGum[i]) {
+        console.log(products[i] +
+            " contains bubble gum");
+    }
+    i = i + 1;
+}
